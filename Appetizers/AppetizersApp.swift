a7265a431ec77026d7744ff43a36bbf41b047e4a
ShiaoC <yan095565@gmail.com>
2024-01-18 01:19:31 +0800

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by 蕭張媛 on 2024/1/18.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
